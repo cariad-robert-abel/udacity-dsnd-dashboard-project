655ac9edffc77b8431bfe6fa7876f9207558c1a8
Robert Abel <robert.abel@cariad.technology>
2026-07-09 16:06:38 +0200

    Dashboard: fix remaining lint issues
    Basically, run autopep8 in aggressive mode and add some manual fixups (e.g. for
    __init__.py).
    
    Signed-off-by: Robert Abel <robert.abel@cariad.technology>

--- report/base_components/matplotlib_viz.py
+++ report/base_components/matplotlib_viz.py
@@ -16,7 +16,7 @@ matplotlib.rcParams['savefig.format'] = 'png'
 
 def matplotlib2fasthtml(func):
     '''
-    Copy of https://github.com/koaning/fh-matplotlib, which is currently hardcoding the 
+    Copy of https://github.com/koaning/fh-matplotlib, which is currently hardcoding the
     image format as jpg. png or svg is needed here.
     '''
     def wrapper(*args, **kwargs):
@@ -44,13 +44,12 @@ class MatplotlibViz(BaseComponent):
     @matplotlib2fasthtml
     def build_component(self, entity_id, model):
         return self.visualization(entity_id, model)
-    
-    
+
     def visualization(self, entity_id, model):
         pass
 
     def set_axis_styling(self, ax, bordercolor='white', fontcolor='white'):
-        
+
         ax.title.set_color(fontcolor)
         ax.xaxis.label.set_color(fontcolor)
         ax.yaxis.label.set_color(fontcolor)
@@ -62,4 +61,3 @@ class MatplotlibViz(BaseComponent):
         for line in ax.get_lines():
             line.set_linewidth(4)
             line.set_linestyle('dashdot')
-
